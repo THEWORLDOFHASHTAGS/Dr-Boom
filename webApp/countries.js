550436a63c0041b64fccf45ee799c0d74f0f145b
sar4eto <sarah_@abv.bg>
2015-10-29 12:56:23 +0100

    Map.
    World map, coloured and half the ID's done.

--- webApp/countries.js
+++ webApp/countries.js
@@ -1,16 +1,25 @@
 var countries = [
     {
-        "country_name": "France",
-        "country_code": "fr",
+        "country_name": "United States of America",
+        "country_code": "Usa",
         "population": 900000
     }
 ]
-
+var temp_array= countries.map(function(item)) {
+                              return item.population;
+                              });
+for (i = 0; i < countries.length; i++) {
+    $('#'+ countries[i].country_code)
+    .css({'fill': 'rgba(11, 104, 170,' 
+         + countries[i].population/highest_value 
+         +')'}).data('country', countries[i]);
+}
 $(document).ready(function(e) {
     
-    $('g').click(function() {
-        var div = $('<div class="model"><h2>Vive la France!</h2><button class="close">close</button></div>').appendTo('body');
-    });
+    $('.map g').click(function() {
+        var country_data= $(this).data('country');
+       $('<div class="model"><h2>Vive la France!</h2><button class="close">close</button></div>').appendTo('body');
+    }); 
     
     $('body').on('click', '.close', function() {
         $('.model').remove();
